2ada7f6ccb805168fa724f310a0046e832a222ab
Zhou Xuhui <zhouxuhui@outlook.com>
2014-08-14 13:29:43 +0800

    commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Side Tree
+
+### Install
+```bash
+# Go redmine/plugins folder and run:
+git clone https://github.com/xuhui/side_tree
+# restart redmine
+```
+
+### Thanks
+
+Many thanks to jquery.bonsai.js ~
---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+Redmine::Plugin.register :side_tree do
+  name 'Side Tree'
+  author 'Zhou Xuhui'
+  description 'Place a collapsible tree into sidebar'
+  version '0.0.1'
+  url 'https://github.com/xuhui/side_tree'
+end
